cdcdad173b83bbc4af3df03ad148caf4b717a58e
sbirkela <scottbirkeland@yahoo.com>
2017-02-24 10:00:20 -0600

    Add files via upload

---
+++ dashboard_template/server.R
@@ -0,0 +1,94 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+#library(rgdal)
+library(RColorBrewer)
+library(googleVis)
+library(leaflet)
+
+
+df <- data.frame(read.csv("inst_df.csv", header = TRUE, sep = ","))
+df <- df[1:5, ]
+df2 <- data.frame(read.csv("inst_df3.csv", header = TRUE, sep = ","))
+
+shinyServer(function(input, output, session) {
+  
+  # icons
+  
+  output$cycle_days <- renderInfoBox({
+    infoBox(
+      " Cycle Days", 56,   #Need to specify the date data change "56"
+      icon = icon("calendar", lib = "font-awesome"),
+      color = "blue"
+    )
+  })
+  
+  output$applicants <- renderInfoBox({
+    infoBox(
+      "Job Seekers", 234, icon = icon("user"), #Need to specify the applicant number data change "34"
+      color = "purple"
+    )
+  })
+  
+  output$pending_candidates <- renderInfoBox({
+    infoBox(
+      "Official Appicants", 10, #Need to specify the pending applicants change "10" 
+      icon = icon("thumbs-up"),
+      color = "yellow"
+    )
+  })
+  
+  output$baseline_score <- renderInfoBox({
+    infoBox(
+      "Ave Baseline Score", 64, #Need to specify the pending applicants change "10" 
+      icon = icon("desktop"),
+      color = "green"
+    )
+  })
+  
+  output$how_hear <- renderPlot({
+    
+    ggplot(data = df) +
+      geom_bar(mapping = aes(x = reorder(How_hear, n),
+                             y = n), stat = "identity", 
+               fill = "Blue") +
+      labs(
+        title = "How Heard About Job Opening", 
+        x = "Recruiting Source", 
+        y = "# of Applicants"
+      ) + coord_flip() +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+  
+  output$app_vol <- renderPlot({
+    ggplot(data = df2) +
+      geom_bar(mapping = aes(x = day_bin, fill = Applicant)) +
+      labs(
+        title = "Applicant Volume", 
+        x = "# Days Posted", 
+        y = "# Applications Submitted"
+      ) +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      scale_fill_discrete(name = "Qualified Applicant")
+  })
+  # add leaflet map
+  output$app_map <- renderLeaflet({
+    latlng_all %>%
+      leaflet() %>%
+      setView(lng = -118.2000, lat = 33.8000, zoom = 7) %>%
+      addTiles() %>% 
+      addCircleMarkers(data=latlng_all,radius=1,group="All", color = "Blue") %>%
+      addCircleMarkers(data=latlng_elect,radius=1,group="Electricians", color = "Blue") %>%
+      addCircleMarkers(data=latlng_inst,radius=1,group="Instrument Technicians", color = "Blue") %>%
+      addCircleMarkers(data=latlng_mech,radius=1,group="Mach/Mill/Mech", color = "Blue") %>%
+      addCircleMarkers(data=latlng_pipe,radius=1,group="Pipefitter", color = "Blue") %>%
+      addCircleMarkers(data=latlng_rigger,radius=1,group="Rigger", color = "Blue") %>%
+      addLayersControl(
+        baseGroups = c("All", "Electricians", "Instrument Technicians", "Mach/Mill/Mech", "Pipefitter", "Rigger"),
+        options = layersControlOptions(collapsed=FALSE)
+      )  
+    
+  })
+  
+})
---
+++ dashboard_template/ui.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(shinydashboard)
+library(shinyBS)
+library(dplyr)
+library(ggplot2)
+#library(rgdal)
+library(RColorBrewer)
+library(googleVis)
+#library(leaflet)
+
+
+
+
+header <- dashboardHeader(title = "Hiring Cycle Analytics")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Applicant Flow", tabName = "dashboard", 
+             icon = icon("dashboard")),
+    
+    menuItem("Map", icon = icon("globe"), tabName = "map"),
+    
+    menuItem("Summary Text", icon = icon("globe"), tabName = "text"),
+    
+    selectInput("job_input", label = "Select a job",
+                choices= c("Instrumentation"),
+                selected = "Instrumentation")
+  )
+)
+
+body <- dashboardBody(
+  bsModal(id = "clientData", title = "Client Data", 
+          trigger = "showData", 
+          verbatimTextOutput("clientdataText")),
+  tabItems(
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              infoBoxOutput(width = 3, "cycle_days"),
+              infoBoxOutput(width = 3, "applicants"),
+              infoBoxOutput(width = 3, "pending_candidates"),
+              infoBoxOutput(width = 3, "baseline_score"),
+            fluidRow(
+              box(width = 6, plotOutput("how_hear")),
+              #box(width = 2, htmlOutput("app_sourc")),
+              box(width = 6, plotOutput("app_vol"))
+            )
+    )
+  ),
+  tabItem(tabName = "map",
+          box(width = 12, leafletOutput("app_map")))
+)
+)
+
+dashboardPage(header, sidebar, body)
