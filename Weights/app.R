fba8260406b2924629e04bb6110f5f5aea89ce59
sbirkela <scottbirkeland@yahoo.com>
2017-06-06 14:57:51 -0500

    Add files via upload

---
+++ Weights/app.R
@@ -0,0 +1,38 @@
+
+library(shiny)
+library(plotly)
+library(ztable)
+library(corrr)
+library(knitr)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   plotlyOutput("plot"),
+   verbatimTextOutput("hover"),
+   verbatimTextOutput("click"),
+   verbatimTextOutput("brush"),
+   verbatimTextOutput("weights")
+)
+
+server <- function(input, output, session) {
+  df <- read.csv("df.csv")
+   output$plot <- renderPlotly({
+         plot_ly(df, x = ~r, y = ~ai, key = ~key) %>% 
+           layout(dragmode = "select")
+   })
+   
+   output$weights <- renderPrint({
+     d1 <- event_data("plotly_click")
+     d2 <- d1$key
+     d <- df %>% filter(key == d2) %>% select(Cognitive:r) %>% fashion(decimals = 3)
+     if (is.null(d)) "Click events appear here (double click to clear)" else d
+   })
+   
+   }
+
+
+
+shinyApp(ui = ui, server = server, options = list(display.mode = "showcase"))
+
+
+
